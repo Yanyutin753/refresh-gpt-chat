37863d14f0b220a8f602c8a5b5cea7b9dc7f816f
Yanyutin753 <3254822118@qq.com>
2024-02-02 10:15:00 +0800

    refresh-gpt-chat v0.0.2
    1. 解决不能传入base64，识别图片的问题
    2. 新增变量**ninja_chatUrl**，使得能同时使用ninja的/v1/chat/completions或者自定义/v1/chat/completions，方便使用
    3. 新增接口**ninja/v1/chat/completions**，扩充接口
    4. 优化访问说明

---
+++ src/main/java/com/refresh/gptChat/controller/CustomErrorController.java
@@ -0,0 +1,35 @@
+package com.refresh.gptChat.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Yangyang
+ * @create 2024-02-01 22:52
+ */
+@RestController
+public class CustomErrorController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public ResponseEntity<String> error() {
+        return new ResponseEntity<>("<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "    <title>Document</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    <p>Thanks you use refresh-gpt-chat</p>\n" +
+                "    <p><a href=\"https://apifox.com/apidoc/shared-4b9a7517-3f80-47a1-84fc-fcf78827a04a\">详细使用文档</a></p>\n" +
+                "    <p><a href=\"https://github.com/Yanyutin753/refresh-gpt-chat\">项目地址</a></p>\n" +
+                "</body>\n" +
+                "</html>\n", HttpStatus.OK);
+    }
+
+}
--- src/main/java/com/refresh/gptChat/controller/chatController.java
+++ src/main/java/com/refresh/gptChat/controller/chatController.java
@@ -1,7 +1,6 @@
 package com.refresh.gptChat.controller;
 
 import com.alibaba.fastjson2.JSON;
-import com.refresh.gptChat.chat.Conversation;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 import org.apache.commons.lang.StringUtils;
@@ -45,6 +44,8 @@ public class chatController {
     private String getAccessTokenUrl;
     @Value("${chatUrl}")
     private String chatUrl;
+    @Value("${ninja_chatUrl}")
+    private String ninja_chatUrl;
 
     @Scheduled(cron = "0 0 0 * * ?")
     private void clearModelsUsage() {
@@ -60,6 +61,7 @@ public class chatController {
     }
 
     /**
+     * 自定义chat接口
      * 请求体不是json 会报Request body is missing or not in JSON format
      * Authorization token缺失  会报Authorization header is missing
      * 无法请求到access_token 会报refresh_token is wrong
@@ -72,7 +74,7 @@ public class chatController {
      * @throws IOException
      */
     @PostMapping(value = "/v1/chat/completions")
-    public CompletableFuture<ResponseEntity<String>> coPilotConversation(HttpServletResponse response, HttpServletRequest request, @org.springframework.web.bind.annotation.RequestBody Conversation conversation) {
+    public CompletableFuture<ResponseEntity<String>> chatConversation(HttpServletResponse response, HttpServletRequest request, @org.springframework.web.bind.annotation.RequestBody Object conversation) {
         return CompletableFuture.supplyAsync(() -> {
             try {
                 if (conversation == null) {
@@ -100,7 +102,7 @@ public class chatController {
                 String json = com.alibaba.fastjson2.JSON.toJSONString(conversation);
                 MediaType JSON = MediaType.get("application/json; charset=utf-8");
                 RequestBody requestBody = RequestBody.create(json, JSON);
-                log.info("请求回复接口："+chatUrl);
+                log.info("请求回复接口：" + chatUrl);
                 Request.Builder requestBuilder = new Request.Builder().url(chatUrl).post(requestBody);
                 headersMap.forEach(requestBuilder::addHeader);
                 Request streamRequest = requestBuilder.build();
@@ -109,11 +111,12 @@ public class chatController {
                     if (!resp.isSuccessful()) {
                         if (resp.code() == 429) {
                             return new ResponseEntity<>("rate limit exceeded", HttpStatus.TOO_MANY_REQUESTS);
-                        }else if(resp.code() == 401){
+                        } else if (resp.code() == 401) {
                             return new ResponseEntity<>("models is not exist", HttpStatus.BAD_REQUEST);
-                        }
-                        else if(resp.code() == 404){
+                        } else if (resp.code() == 404) {
                             return new ResponseEntity<>("404", HttpStatus.NOT_FOUND);
+                        } else if (resp.code() == 500) {
+                            return new ResponseEntity<>("INTERNAL SERVER ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
                         }
                         else {
                             String token = getAccessToken(refresh_token);
@@ -122,7 +125,7 @@ public class chatController {
                             }
                             refreshTokenList.put(refresh_token, token);
                             log.info("assess_token过期，refreshTokenList重置化成功！");
-                            againConversation(response, conversation, token);
+                            againConversation(response, conversation, token, chatUrl);
                         }
                     } else {
                         // 流式和非流式输出
@@ -143,6 +146,88 @@ public class chatController {
         });
     }
 
+    /**
+     * ninja_chat接口
+     * 请求体不是json 会报Request body is missing or not in JSON format
+     * Authorization token缺失  会报Authorization header is missing
+     * 无法请求到access_token 会报refresh_token is wrong
+     *
+     * @param response
+     * @param request
+     * @param conversation
+     * @return
+     * @throws JSONException
+     * @throws IOException
+     */
+    @PostMapping(value = "ninja/v1/chat/completions")
+    public CompletableFuture<ResponseEntity<String>> ninjaConversation(HttpServletResponse response, HttpServletRequest request, @org.springframework.web.bind.annotation.RequestBody Object conversation) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                if (conversation == null) {
+                    return new ResponseEntity<>("Request body is missing or not in JSON format", HttpStatus.BAD_REQUEST);
+                }
+                String authorizationHeader = StringUtils.trimToNull(request.getHeader("Authorization"));
+                String refresh_token;
+                if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                    refresh_token = authorizationHeader.substring(7);
+                } else {
+                    return new ResponseEntity<>("Authorization header is missing", HttpStatus.UNAUTHORIZED);
+                }
+                if (!refreshTokenList.containsKey(refresh_token)) {
+                    String token = getAccessToken(refresh_token);
+                    if (token == null) {
+                        return new ResponseEntity<>("refresh_token is wrong", HttpStatus.UNAUTHORIZED);
+                    }
+                    refreshTokenList.put(refresh_token, token);
+                    log.info("refreshTokenList初始化成功！");
+                }
+                String access_token = refreshTokenList.get(refresh_token);
+                Map<String, String> headersMap = new HashMap<>();
+                //添加头部
+                addHeader(headersMap, access_token);
+                String json = com.alibaba.fastjson2.JSON.toJSONString(conversation);
+                MediaType JSON = MediaType.get("application/json; charset=utf-8");
+                RequestBody requestBody = RequestBody.create(json, JSON);
+                log.info("请求回复接口：" + ninja_chatUrl);
+                Request.Builder requestBuilder = new Request.Builder().url(ninja_chatUrl).post(requestBody);
+                headersMap.forEach(requestBuilder::addHeader);
+                Request streamRequest = requestBuilder.build();
+                try (Response resp = client.newCall(streamRequest).execute()) {
+                    log.info(resp.toString());
+                    if (!resp.isSuccessful()) {
+                        if (resp.code() == 429) {
+                            return new ResponseEntity<>("rate limit exceeded", HttpStatus.TOO_MANY_REQUESTS);
+                        } else if (resp.code() == 401) {
+                            return new ResponseEntity<>("models is not exist", HttpStatus.BAD_REQUEST);
+                        } else if (resp.code() == 404) {
+                            return new ResponseEntity<>("404", HttpStatus.NOT_FOUND);
+                        } else {
+                            String token = getAccessToken(refresh_token);
+                            if (token == null) {
+                                return new ResponseEntity<>("refresh_token is wrong", HttpStatus.UNAUTHORIZED);
+                            }
+                            refreshTokenList.put(refresh_token, token);
+                            log.info("assess_token过期，refreshTokenList重置化成功！");
+                            againConversation(response, conversation, token, ninja_chatUrl);
+                        }
+                    } else {
+                        // 流式和非流式输出
+                        outPutChat(response, resp, conversation);
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        }, executor).orTimeout(6, TimeUnit.MINUTES).exceptionally(ex -> {
+            // 处理超时或其他异常
+            if (ex instanceof TimeoutException) {
+                return new ResponseEntity<>("Request timed out", HttpStatus.REQUEST_TIMEOUT);
+            } else {
+                return new ResponseEntity<>("An error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        });
+    }
 
     /**
      * 如发现token过期
@@ -153,7 +238,7 @@ public class chatController {
      * @param access_token
      * @return
      */
-    public Object againConversation(HttpServletResponse response, @org.springframework.web.bind.annotation.RequestBody Conversation conversation, String access_token) {
+    public Object againConversation(HttpServletResponse response, @org.springframework.web.bind.annotation.RequestBody Object conversation, String access_token, String chat_url) {
         try {
             Map<String, String> headersMap = new HashMap<>();
             //添加头部
@@ -163,16 +248,15 @@ public class chatController {
             MediaType JSON = MediaType.get("application/json; charset=utf-8");
             RequestBody requestBody = RequestBody.create(json, JSON);
             Request streamRequest = new Request.Builder()
-                    .url(chatUrl)
-                    .post(requestBody )
+                    .url(chat_url)
+                    .post(requestBody)
                     .addHeader("Authorization", "Bearer " + access_token)
                     .addHeader("Content-Type", "application/json")
                     .build();
             try (Response resp = client.newCall(streamRequest).execute()) {
                 if (!resp.isSuccessful()) {
                     return new ResponseEntity<>("refresh_token is wrong Or your network is wrong", HttpStatus.UNAUTHORIZED);
-                }
-                else {
+                } else {
                     // 流式和非流式输出
                     outPutChat(response, resp, conversation);
                 }
@@ -229,10 +313,11 @@ public class chatController {
      * @param resp
      * @param conversation
      */
-    private void outPutChat(HttpServletResponse response, Response resp, Conversation conversation) {
+    private void outPutChat(HttpServletResponse response, Response resp, Object conversation) {
         try {
-            Boolean isStream = conversation.getStream();
-            if (isStream != null && isStream) {
+            JSONObject jsonObject = new JSONObject(com.alibaba.fastjson2.JSON.toJSONString(conversation));
+            boolean isStream = jsonObject.optBoolean("stream", false);
+            if (isStream) {
                 response.setContentType("text/event-stream; charset=UTF-8");
             } else {
                 response.setContentType("application/json; charset=utf-8");
@@ -248,13 +333,12 @@ public class chatController {
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
         }
     }
 
     private void addHeader(Map<String, String> headersMap, String access_token) {
-        headersMap.put("Accept-Encoding", "gzip, deflate, br");
-        headersMap.put("Accept", "*/*");
         headersMap.put("Authorization", "Bearer " + access_token);
-        headersMap.put("User-Agent","PostmanRuntime/7.36.1");
     }
 }
